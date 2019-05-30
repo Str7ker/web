de619ed057469bb1115a9a243cd978c095ced3f2
Str7ker <butrim.ivan@mail.ru>
2019-05-30 18:02:22 +0400

    30.05

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+# admin.site.register(Company)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'email', 'address_fact', 'vk', 'insta')
+admin.site.register(Contact,ContactAdmin)
--- index/models.py
+++ index/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    phone = models.CharField(max_length=25, blank=True)
+    email = models.CharField(max_length=30, blank=True)
+    address_fact = models.CharField(max_length=250, blank=True)
+    vk = models.CharField(max_length=150, blank=True)
+    insta = models.CharField(max_length=150, blank=True)
+
+    class Meta:
+        verbose_name = 'Контакты'
+        verbose_name_plural = 'Контакты'
